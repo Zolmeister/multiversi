0a4926202f0950443d9c630884fcad0703121d51
Zoli Kahan <zolikahan@gmail.com>
2012-12-27 18:24:00 -0600

    properly formatted javascript

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// @formatter:off
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
@@ -5,42 +6,44 @@ var express = require('express')
   , cons = require('consolidate')
    
   , socketConnect = require('./sockets');
+// @formatter:on
 
-var app = express()
-var server = http.createServer(app)
-io = require('socket.io').listen(server)
+var app = express();
+var server = http.createServer(app);
+io = require('socket.io').listen(server);
 io.configure('development', function() {
-    io.set('log level', 1);
+	io.set('log level', 1);
 });
-
 
 app.engine('dust', cons.dust);
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000)
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'dust'); //dust.js default
-    app.set('view options', {
-        layout: false
-    }); //disable layout default
-    app.locals({
-        layout: false
-    })
-  app.use(express.logger('dev'))
-  app.use(express.bodyParser())
-  app.use(express.methodOverride())
-  app.use(app.router)
-  app.use(express.static(path.join(__dirname, 'public')))
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'dust');
+	//dust.js default
+	app.set('view options', {
+		layout : false
+	});
+	//disable layout default
+	app.locals({
+		layout : false
+	});
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 })
 
-app.configure('development', function(){
-  app.use(express.errorHandler())
-})
+app.configure('development', function() {
+	app.use(express.errorHandler());
+});
 
-app.get('/', routes.index)
+app.get('/', routes.index);
 
 io.sockets.on('connection', socketConnect);
 
-server.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'))
-})
+server.listen(app.get('port'), function() {
+	console.log("Express server listening on port " + app.get('port'));
+});
