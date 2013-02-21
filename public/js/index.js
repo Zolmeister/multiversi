df37a44bff670aee7e2954c3d95179b62a98b807
Brad Zeis <bradzeis@gmail.com>
2013-02-20 21:48:05 -0600

    Fixed screen name overlay.

--- public/js/index.js
+++ public/js/index.js
@@ -31,25 +31,13 @@ function openScreenNameOverlay() {
     $("#screenNameOverlay").show();
 }
 
-//TODO: Brad
-//instead of this, make it a form input and handle submission with jQuery/zepto
-//see chat input for example
-function onScreenNameInputKeyPress(e) {
-    if (!e) e = window.event;
-    var keyCode = e.keyCode || e.which;
-    if (keyCode == '13') {
-        // Enter pressed
-        applyScreenName();
-    }
-}
-
 function applyScreenName(form) {
-    form = $(form);
-    var name = form.serializeArray()[0].value;
-    // form.find("input").val("");
-    
-    globalConnect().setName(name);
-    // globalConnect().setName($("#screenNameInput").val())
-    $("#screenNameOverlay").hide()
+    var name = $("#screenNameInput").val();
+    if (name === "" || name === "Guest") {
+        // TODO: Yell at user
+    } else {
+        globalConnect().setName($("#screenNameInput").val())
+        $("#screenNameOverlay").hide()
+    }
 }
 
