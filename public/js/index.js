463420b1381b217a2286a16f891ccf8500ae1b8c
Zoli Kahan <zolikahan@gmail.com>
2013-02-23 15:16:43 -0600

    added basic social buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,39 @@
 $(function() {
-    
+
     var windowEvent = function(e) {
         console.log(e)
         if (e.state === "room") {
             $("#roomView").stop().animate({
-                top: "0%"
+                top : "0%"
             }, 1000)
             $("#homePageView").stop().fadeOut();
-        }
-        else if(e.state === "lobby"){
+        } else if (e.state === "lobby") {
             $("#roomView").stop().animate({
-                top: "100%"
-            }, 1000, function(){
+                top : "100%"
+            }, 1000, function() {
                 lobby.leaveRoom()
             })
             $("#homePageView").stop().fadeIn();
         }
     }
     window.history.onpushstate = windowEvent;
-    
+
     var pushState = window.history.pushState;
-    
+
     window.history.pushState = function(state) {
-        if (typeof window.history.onpushstate == "function") {
-            window.history.onpushstate({state: state});
+        if ( typeof window.history.onpushstate == "function") {
+            window.history.onpushstate({
+                state : state
+            });
         }
         return pushState.apply(window.history, arguments);
     }
-    
     globalConnect = ko.observable(new Connect());
     lobby = new Lobby();
-    if(lobby.dynamicJoin){
+    if (lobby.dynamicJoin) {
         $("#homePageView").hide();
         $("#roomView").stop().css({
-                top: "0%"
+            top : "0%"
         })
     }
     ko.applyBindings(lobby, $("#homePageView")[0]);
@@ -41,11 +41,17 @@ $(function() {
     window.addEventListener('popstate', windowEvent);
     window.addEventListener('pushstate', windowEvent);
 
+    FB.init({
+        appId : '128568713986892',
+        xfbml : true,
+        cookie : true
+    });
+
 });
 
 function inRoom() {
     var num = window.location.href.split("/").pop();
-    if (typeof num === "string" && num.length>=3) {
+    if ( typeof num === "string" && num.length >= 3) {
         return num;
     }
     return false;
@@ -77,3 +83,10 @@ function applyScreenName(form) {
     }
 }
 
+function facebookSend() {
+    FB.ui({
+        method : 'send',
+        name : 'Join me in a game of Multiversi',
+        link : lobby.room().shareUrl,
+    });
+}
